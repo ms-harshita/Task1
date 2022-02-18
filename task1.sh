0b997d3663b168e04dd8a0aa8d778a2795d40bf9
harshita sachdeva <harshita.sachdeva@innovaccer.com>
2022-02-18 16:38:14 +0530

    modified

--- task1.sh
+++ task1.sh
@@ -3,13 +3,13 @@ echo "Hi, Enter student's name"
 read name
 
 echo "Enter the marks in 5 subjects"
-read A
-read B
-read C
-read D
-read E
+read a
+read b
+read c
+read d
+read e
 total=500
-((sum= A+B+C+D+E))
+((sum= a+b+c+d+e))
 result=$((100*$sum/$total))
 echo "Name:$name  Percentage of the student is=$result" >>result.txt
 echo "Name:$name  Percentage of the student is=$result" 
